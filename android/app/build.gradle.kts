2e879c0991db891ccf7f982c3509ab68137432c5
aerkan2527 <abdullaherkan8@gmail.com>
2025-08-29 20:12:10 +0300

    Add keystore configuration and app icons from kangaroom-veli project

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.kangaroom"
+    namespace = "com.missoft.kangaroom"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -19,9 +19,18 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("../../kangaroom.jks")
+            storePassword = "Kanga2527."
+            keyAlias = "veli"
+            keyPassword = "Kanga2527."
+        }
+    }
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.kangaroom"
+        applicationId = "com.missoft.kangaroom"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
     minSdk = 23
@@ -32,9 +41,10 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            isShrinkResources = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 }
